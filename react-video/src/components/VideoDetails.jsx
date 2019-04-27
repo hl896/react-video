730b1598fa69f75bc7c262d71a8f40a9e3063863
yeung.li <yeung.li@hinterlands.tech>
2019-04-27 11:58:02 +1000

    test

--- react-video/src/components/VideoDetails.jsx
+++ react-video/src/components/VideoDetails.jsx
@@ -18,10 +18,18 @@ class VideoDetails extends React.Component{
         }
         // const details= video.snippet.title;
         console.log(this.props.videodetails.snippet.title)
+        const videoSrc= `https://www.youtube.com/embed/${this.props.videodetails.id.videoId}`
         return(
             <div>
-                {this.props.videodetails.snippet.title}
+                <div className="ui embed" >
+                    <iframe src={videoSrc} frameborder="0"></iframe>
+                </div>
+                <div className="ui segment">
+                    <h4 className="ui header">{this.props.videodetails.snippet.title}</h4>
+                    <p>{this.props.videodetails.snippet.description}</p>
+                </div>
             </div>
+                
         );
         
         
